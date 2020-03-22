da99c77be55972e9f9720d6566fb19f8e1bc394b
Samfield Hawb Bassey <samfield4sure@gmail.com>
2020-03-22 19:41:09 +0100

    added fonticon and buttons to change langauge

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <button v-for="entry in languages" :key="entry.title" @click="changeLocale(entry.language)">
+        <flag :iso="entry.flag" v-bind:squared="false" />
+        {{entry.title}}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import i18n from '@/plugins/i18n';
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      languages: [
+        { flag: "us", language: "en", title: "English" },
+        { flag: "es", language: "es", title: "Español" }
+      ]
+    };
+  },
   components: {
     HelloWorld
-  }
-}
+  },
+  methods: {    changeLocale(locale) {        i18n.locale = locale;    }}
+};
 </script>
 
 <style>
@@ -25,4 +41,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+button {    padding: 15px;    border: 1px solid green;    font-size: 18px;    margin: 15px;}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import i18n from '@/plugins/i18n';
+import FlagIcon from 'vue-flag-icon';
+Vue.use(FlagIcon);
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
